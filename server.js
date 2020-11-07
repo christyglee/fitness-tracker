2f8f442e99a920c779e4fc5ea84b7e3ea03a70ab
unknown <christy.g.lee@gmail.com>
2020-11-07 15:53:07 -0800

    server.js connection

---
+++ server.js
@@ -0,0 +1,28 @@
+//Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
